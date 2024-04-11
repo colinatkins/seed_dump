28317427c8056ce2542a4bea37613a76e6b94755
Colin Atkins <atkins@hey.com>
2024-04-11 21:26:10 +0200

    Update dump_methods.rb

--- lib/seed_dump/dump_methods.rb
+++ lib/seed_dump/dump_methods.rb
@@ -36,7 +36,7 @@ class SeedDump
 
     def value_to_s(value)
       value = case value
-              when BigDecimal, IPAddr, ActionText::Content
+              when BigDecimal, IPAddr, ActionText::Content, ActionText::EncryptedRichText
                 value.to_s
               when Date, Time, DateTime
                 value.to_s(:db)
